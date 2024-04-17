ea1d32f3bd1ce6ff78cb8e899d6f744b3d2b14dd
IanWohlhieter <161521090+IanWohlhieter@users.noreply.github.com>
2024-04-17 19:43:41 -0400

    Update snake.c
    Snake moves with arrow keys. Proper exit upon head crossing pit bounds.

--- snake.c
+++ snake.c
@@ -7,53 +7,52 @@
 #include <curses.h>
 
 int main(int ac, char *av[]){
-	int snake_length = 5;
+int snake_length = 5;
     initscr();
     cbreak();
-	curs_set(0); // sets the cursor to invisible (0-2)=(invis-highvis)
-    printf("The screen is %d rows, by %d columns.\n", LINES-1, COLS);
+    printf("The screen is %d rows, by %d columns.\r\nPress any key to begin!", LINES-1, COLS);
     getchar();
     clear();
-	nodelay(stdscr, TRUE);
+    curs_set(0);
     keypad(stdscr, TRUE);
+    nodelay(stdscr, TRUE);
 	noecho();
 	int x_coord = 1+snake_length;
 	int y_coord = 1;
 	int direction = KEY_RIGHT;
-	while (0<x_coord && x_coord<COLS && 0<y_coord && y_coord<LINES){ // while (within bounds of pit)
+	while (0<x_coord && x_coord<COLS-1 && 0<y_coord && y_coord<LINES-1){ // while (snake head is within bounds of pit){
 		clear();
 		border(ACS_VLINE, ACS_VLINE, ACS_HLINE, ACS_HLINE, ACS_ULCORNER, ACS_URCORNER, ACS_LLCORNER, ACS_LRCORNER);
-        move(y_coord, x_coord);
-        for (int p=0; p<snake_length; p++){ // print the snake (from head to tail)
-			move(y_coord, x_coord+snake_length-p);
+		move(y_coord, x_coord);
+		for (int p=0; p<snake_length; p++){ // print the snake (from head to tail)
+			move(y_coord, x_coord-p);
 			addch(ACS_BLOCK);
 		}
-        refresh();
+		refresh();
 		int curr_dir = direction;
 		usleep(10000);
-        direction = getch();
-        switch (direction){
-            case KEY_DOWN: // 402
-                y_coord++;
-                break;
-			case KEY_UP: // 403
-                y_coord--;
-                break;
-            case KEY_LEFT: // 404
-                x_coord--;
-                break;
-            case KEY_RIGHT: // 405
-                x_coord++;
-                break;
-            case 'k':
-                endwin();
-                return 0;
-            default:
-				direction = curr_dir;
-                break;
-        }
+		direction = getch();
+		switch (direction){
+		    case KEY_DOWN: // 402
+			y_coord++;
+			break;
+		case KEY_UP: // 403
+			y_coord--;
+			break;
+		case KEY_LEFT: // 404
+			x_coord--;
+			break;
+		case KEY_RIGHT: // 405
+			x_coord++;
+			break;
+		case 'k':
+			endwin();
+			return 0;
+		default:
+			direction = curr_dir;
+			break;
+		}
     }
-    getchar();
     endwin();
     return 0;
 }
